cf8d4fa8312c093b3b988d32831893ecfc29961b
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-01 15:17:27 +0700

    tambah konfigurasi spring security

---
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,62 @@
+package com.rizki.mufrizal.pelayanan.labti.configuration;
+
+import com.rizki.mufrizal.pelayanan.labti.service.impl.UserAuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since May 1, 2016
+ * @Time 8:56:31 AM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti.configuration
+ *
+ */
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserAuthenticationService userAuthenticationService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userAuthenticationService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("webjars/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout")
+                .deleteCookies("JSESSIONID");
+    }
+
+}
