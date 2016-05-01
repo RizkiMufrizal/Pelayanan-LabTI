809041d5e8515e071144793507fe83fca23b25f3
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-01 15:14:57 +0700

    tambah praktikum repository

---
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/repository/PraktikumRepository.java
@@ -0,0 +1,17 @@
+package com.rizki.mufrizal.pelayanan.labti.repository;
+
+import com.rizki.mufrizal.pelayanan.labti.domain.Praktikum;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since May 1, 2016
+ * @Time 9:07:50 AM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti.repository
+ *
+ */
+public interface PraktikumRepository extends PagingAndSortingRepository<Praktikum, String> {
+
+}
