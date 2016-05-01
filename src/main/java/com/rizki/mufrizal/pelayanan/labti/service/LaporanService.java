d1502825d9daaf6c96dd076d5a02bb10efccf34e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-01 15:15:21 +0700

    tambah laporan service

---
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/service/LaporanService.java
@@ -0,0 +1,26 @@
+package com.rizki.mufrizal.pelayanan.labti.service;
+
+import com.rizki.mufrizal.pelayanan.labti.domain.Laporan;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Apr 30, 2016
+ * @Time 9:35:40 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti.service
+ *
+ */
+public interface LaporanService {
+
+    public void simpanLaporan(Laporan laporan);
+
+    public void ubahLaporan(Laporan laporan);
+
+    public void hapusLaporan(String idLaporan);
+
+    public Laporan getLaporan(String idLaporan);
+
+    public Iterable<Laporan> getLaporans();
+
+}
