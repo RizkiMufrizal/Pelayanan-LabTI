9478a6ad3854c82896afcb167d31ec0a120007d2
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-01 15:18:00 +0700

    tambah page controller

---
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/controller/PageController.java
@@ -0,0 +1,24 @@
+package com.rizki.mufrizal.pelayanan.labti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since May 1, 2016
+ * @Time 12:41:53 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti.controller
+ *
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "IndexView";
+    }
+
+}
