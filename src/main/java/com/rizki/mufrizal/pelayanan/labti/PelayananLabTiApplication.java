d89b88efc10b546b1d50701fa60f2418a5042e68
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-01 15:18:26 +0700

    update app main class

--- src/main/java/com/rizki/mufrizal/pelayanan/labti/PelayananLabTiApplication.java
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/PelayananLabTiApplication.java
@@ -3,10 +3,19 @@ package com.rizki.mufrizal.pelayanan.labti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Apr 30, 2016
+ * @Time 8:44:32 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti
+ *
+ */
 @SpringBootApplication
 public class PelayananLabTiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PelayananLabTiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PelayananLabTiApplication.class, args);
+    }
 }
