a66e4bec2588914a913a380f31d962a98d53eb9c
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-01 15:15:30 +0700

    tambah praktikum service

---
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/service/PraktikumService.java
@@ -0,0 +1,26 @@
+package com.rizki.mufrizal.pelayanan.labti.service;
+
+import com.rizki.mufrizal.pelayanan.labti.domain.Praktikum;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since May 1, 2016
+ * @Time 9:10:37 AM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti.service
+ *
+ */
+public interface PraktikumService {
+
+    public void simpanPraktikum(Praktikum praktikum);
+
+    public void ubahPraktikum(Praktikum praktikum);
+
+    public void hapusPraktikum(Praktikum praktikum);
+
+    public Praktikum getPraktikum(String idPraktikum);
+
+    public Iterable<Praktikum> getPraktikums();
+
+}
