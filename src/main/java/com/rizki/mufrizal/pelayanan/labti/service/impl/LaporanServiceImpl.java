8de104f38d00b10a674ca5c5df565b8f43d48563
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-01 18:43:46 +0700

    tambah cache

--- src/main/java/com/rizki/mufrizal/pelayanan/labti/service/impl/LaporanServiceImpl.java
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/service/impl/LaporanServiceImpl.java
@@ -4,6 +4,9 @@ import com.rizki.mufrizal.pelayanan.labti.domain.Laporan;
 import com.rizki.mufrizal.pelayanan.labti.repository.LaporanRepository;
 import com.rizki.mufrizal.pelayanan.labti.service.LaporanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,37 +21,49 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 @Transactional(readOnly = true)
+@CacheConfig(cacheNames = "laporan")
 public class LaporanServiceImpl implements LaporanService {
 
     @Autowired
     private LaporanRepository laporanRepository;
 
+    @CacheEvict(allEntries = true, key = "#laporan")
     @Transactional
     @Override
     public void simpanLaporan(Laporan laporan) {
         laporanRepository.save(laporan);
     }
 
+    @CacheEvict(allEntries = true, key = "#laporan")
     @Transactional
     @Override
     public void ubahLaporan(Laporan laporan) {
         laporanRepository.save(laporan);
     }
 
+    @CacheEvict(allEntries = true, key = "#idLaporan")
     @Transactional
     @Override
     public void hapusLaporan(String idLaporan) {
         laporanRepository.delete(idLaporan);
     }
 
+    @Cacheable
     @Override
     public Laporan getLaporan(String idLaporan) {
         return laporanRepository.findOne(idLaporan);
     }
 
+    @Cacheable
     @Override
     public Iterable<Laporan> getLaporans() {
         return laporanRepository.findAll();
+    }
+
+    @Cacheable
+    @Override
+    public Long countByNpmAndPraktikumIdPraktikum(String npm, String idPraktikum) {
+        return laporanRepository.countByNpmAndPraktikumIdPraktikum(npm, idPraktikum);
     }
 
 }
--- src/main/java/com/rizki/mufrizal/pelayanan/labti/service/impl/PraktikumServiceImpl.java
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/service/impl/PraktikumServiceImpl.java
@@ -4,6 +4,9 @@ import com.rizki.mufrizal.pelayanan.labti.domain.Praktikum;
 import com.rizki.mufrizal.pelayanan.labti.repository.PraktikumRepository;
 import com.rizki.mufrizal.pelayanan.labti.service.PraktikumService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,34 +21,40 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 @Transactional(readOnly = true)
+@CacheConfig(cacheNames = "praktikum")
 public class PraktikumServiceImpl implements PraktikumService {
 
     @Autowired
     private PraktikumRepository praktikumRepository;
 
+    @CacheEvict(allEntries = true, key = "#praktikum")
     @Transactional
     @Override
     public void simpanPraktikum(Praktikum praktikum) {
         praktikumRepository.save(praktikum);
     }
 
+    @CacheEvict(allEntries = true, key = "#praktikum")
     @Transactional
     @Override
     public void ubahPraktikum(Praktikum praktikum) {
         praktikumRepository.save(praktikum);
     }
 
+    @CacheEvict(allEntries = true, key = "#idPraktikum")
     @Transactional
     @Override
-    public void hapusPraktikum(Praktikum praktikum) {
-        praktikumRepository.delete(praktikum);
+    public void hapusPraktikum(String idPraktikum) {
+        praktikumRepository.delete(idPraktikum);
     }
 
+    @Cacheable
     @Override
     public Praktikum getPraktikum(String idPraktikum) {
         return praktikumRepository.findOne(idPraktikum);
     }
 
+    @Cacheable
     @Override
     public Iterable<Praktikum> getPraktikums() {
         return praktikumRepository.findAll();
