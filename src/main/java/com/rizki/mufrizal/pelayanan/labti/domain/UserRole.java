891527662ab22021db33ae904d21253967af4078
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-01 15:14:28 +0700

    tambah model user role

---
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/domain/UserRole.java
@@ -0,0 +1,87 @@
+package com.rizki.mufrizal.pelayanan.labti.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since May 1, 2016
+ * @Time 9:00:47 AM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti.domain
+ *
+ */
+@Entity
+@Table(name = "tb_user_role")
+public class UserRole implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id_user_role", length = 150)
+    private String idUserRole;
+
+    @Column(name = "role", length = 10)
+    private String role;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
+    @JoinColumn(name = "email", nullable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
+    private User user;
+
+    /**
+     * @return the idUserRole
+     */
+    public String getIdUserRole() {
+        return idUserRole;
+    }
+
+    /**
+     * @param idUserRole the idUserRole to set
+     */
+    public void setIdUserRole(String idUserRole) {
+        this.idUserRole = idUserRole;
+    }
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
