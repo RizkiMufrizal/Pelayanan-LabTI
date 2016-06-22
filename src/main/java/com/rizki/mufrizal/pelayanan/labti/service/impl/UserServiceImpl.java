a75c8064b7fb79d63f150df07f9d91d5eaccf598
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-23 00:34:07 +0700

    update user service impl

--- src/main/java/com/rizki/mufrizal/pelayanan/labti/service/impl/UserServiceImpl.java
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.rizki.mufrizal.pelayanan.labti.service.impl;
 
 import com.rizki.mufrizal.pelayanan.labti.domain.User;
+import com.rizki.mufrizal.pelayanan.labti.domain.UserRole;
 import com.rizki.mufrizal.pelayanan.labti.repository.UserRepository;
 import com.rizki.mufrizal.pelayanan.labti.service.UserService;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,6 +31,37 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUser(String username) {
         return userRepository.findOne(username);
+    }
+
+    @Transactional
+    @Override
+    public void simpanUser(User user) {
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        UserRole userRole = new UserRole();
+        userRole.setRole("ROLE_ADMIN");
+        userRole.setUser(user);
+
+        Set<UserRole> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+
+        user.setUserRoles(userRoles);
+        user.setEnable(Boolean.TRUE);
+
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(String username) {
+        userRepository.delete(username);
+    }
+
+    @Override
+    public Iterable<User> users() {
+        return userRepository.findAll();
     }
 
 }
