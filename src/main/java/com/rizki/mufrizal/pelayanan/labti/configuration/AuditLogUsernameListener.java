480478c676393d9e87a84aeb79ee7a9b0bf9d440
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-22 22:53:36 +0700

    tambah audit log username listener

---
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/configuration/AuditLogUsernameListener.java
@@ -0,0 +1,27 @@
+package com.rizki.mufrizal.pelayanan.labti.configuration;
+
+import com.rizki.mufrizal.pelayanan.labti.domain.AuditLogUsername;
+import org.hibernate.envers.RevisionListener;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jun 22, 2016
+ * @Time 9:59:31 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti.configuration
+ *
+ */
+public class AuditLogUsernameListener implements RevisionListener {
+
+    @Override
+    public void newRevision(Object object) {
+        AuditLogUsername auditLogUsername = (AuditLogUsername) object;
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        auditLogUsername.setUsername(username);
+    }
+
+}
