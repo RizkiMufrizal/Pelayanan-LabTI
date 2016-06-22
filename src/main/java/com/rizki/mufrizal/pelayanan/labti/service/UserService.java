50873fb9e4c22943d0589fa45250a7352ba92eca
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-22 21:25:02 +0700

    tambah user service

---
+++ src/main/java/com/rizki/mufrizal/pelayanan/labti/service/UserService.java
@@ -0,0 +1,19 @@
+package com.rizki.mufrizal.pelayanan.labti.service;
+
+import com.rizki.mufrizal.pelayanan.labti.domain.User;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jun 22, 2016
+ * @Time 8:30:20 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package com.rizki.mufrizal.pelayanan.labti.service
+ *
+ */
+public interface UserService {
+
+    User getUser(String username);
+
+}
