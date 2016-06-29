5e15294e6e2a4bd0e96f84b86d3785bc74c56df8
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-29 22:19:24 +0700

    tambah delete user

---
+++ src/test/resources/data/DeleteUser.sql
@@ -0,0 +1,13 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jun 29, 2016
+ * @Time 9:39:21 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/SQLTemplate.sql.
+ *
+ */
+
+delete from tb_user_role;
+delete from tb_user;
