d233bfb4034801ba3974326142673ab7aac68f5b
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-29 23:17:51 +0700

    tambah delete laporan

---
+++ src/test/resources/data/DeleteLaporan.sql
@@ -0,0 +1,14 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jun 29, 2016
+ * @Time 10:23:01 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/SQLTemplate.sql.
+ *
+ */
+delete from tb_laporan;
+delete from tb_user_role;
+delete from tb_user;
+delete from tb_praktikum;
