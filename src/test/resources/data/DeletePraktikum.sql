a7943f232b6bad8fac7d791f3ed320f32a4245b1
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-29 21:32:29 +0700

    tambah delete praktikum

---
+++ src/test/resources/data/DeletePraktikum.sql
@@ -0,0 +1,11 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jun 29, 2016
+ * @Time 8:36:56 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/SQLTemplate.sql.
+ *
+ */
+delete from tb_praktikum;
