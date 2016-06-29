96b4bbbff52f58aeb1f343ed05e9a1e2ee6f7120
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-29 21:32:22 +0700

    tambah insert praktikum

---
+++ src/test/resources/data/InsertPraktikum.sql
@@ -0,0 +1,25 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jun 29, 2016
+ * @Time 8:36:48 PM
+ * @Encoding UTF-8
+ * @Project Pelayanan-LabTI
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/SQLTemplate.sql.
+ *
+ */
+
+INSERT INTO tb_praktikum(id_praktikum, batas_jumlah_tidak_masuk, jumlah_pertemuan, nama_praktikum)
+VALUES('1', 2, 7, 'Rekayasa Perangkat Lunak 2');
+
+INSERT INTO tb_praktikum(id_praktikum, batas_jumlah_tidak_masuk, jumlah_pertemuan, nama_praktikum)
+VALUES('2', 1, 4, 'Pemrograman Jaringan');
+
+INSERT INTO tb_praktikum(id_praktikum, batas_jumlah_tidak_masuk, jumlah_pertemuan, nama_praktikum)
+VALUES('3', 1, 4, 'Sistem Multimedia');
+
+INSERT INTO tb_praktikum(id_praktikum, batas_jumlah_tidak_masuk, jumlah_pertemuan, nama_praktikum)
+VALUES('4', 2, 7, 'Interaksi Manusia Dan Komputer');
+
+INSERT INTO tb_praktikum(id_praktikum, batas_jumlah_tidak_masuk, jumlah_pertemuan, nama_praktikum)
+VALUES('5', 2, 7, 'Sistem Basis Data 2');
